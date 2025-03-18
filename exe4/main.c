ead624e4ac17ed7368cda490a2cdbd6f656ad24d
Enzo <enzopj@al.insper.edu.br>
2025-03-18 18:49:54 -0300

    Adiciona exercícios 1,2,3 e 4 para controle de LEDs e botões verdes, implementando semáforos e filas para gerenciamento de eventos.

--- exe4/main.c
+++ exe4/main.c
@@ -1,23 +1,26 @@
 #include <FreeRTOS.h>
 #include <task.h>
-#include <semphr.h>
 #include <queue.h>
-
 #include "pico/stdlib.h"
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
-
 const int LED_PIN_R = 4;
 const int LED_PIN_G = 6;
 
-QueueHandle_t xQueueButId;
-SemaphoreHandle_t xSemaphore_r;
+QueueHandle_t xQueueButId_r;
+QueueHandle_t xQueueButId_g;
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        xSemaphoreGiveFromISR(xSemaphore_r, 0);
+
+void gpio_callback(uint gpio, uint32_t events) {
+    if (events == GPIO_IRQ_EDGE_FALL) {
+        int valor = 100;               
+        if (gpio == BTN_PIN_R) {
+            xQueueSendFromISR(xQueueButId_r, &valor, 0);
+        } else if (gpio == BTN_PIN_G) {
+            xQueueSendFromISR(xQueueButId_g, &valor, 0);
+        }
     }
 }
 
@@ -25,14 +28,21 @@ void led_1_task(void *p) {
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
-    int delay = 0;
+    int delay = 0; // Delay inicial em ms
+    int dummy;
 
     while (true) {
-        if (xQueueReceive(xQueueButId, &delay, 0)) {
-            printf("%d\n", delay);
+        // Aguarda até 500ms por um evento vindo da fila do botão vermelho
+        if (xQueueReceive(xQueueButId_r, &dummy, pdMS_TO_TICKS(500)) == pdPASS) {
+            if (delay < 1000)
+                delay += 100;
+            else
+                delay = 100;
+            printf("Red LED delay updated to: %d ms\n", delay);
         }
-
-        if (delay > 0) {
+        if (delay > 0)
+        {
+            // Pisca o LED verde com o delay atual
             gpio_put(LED_PIN_R, 1);
             vTaskDelay(pdMS_TO_TICKS(delay));
             gpio_put(LED_PIN_R, 0);
@@ -41,36 +51,56 @@ void led_1_task(void *p) {
     }
 }
 
-void btn_1_task(void *p) {
-    gpio_init(BTN_PIN_R);
-    gpio_set_dir(BTN_PIN_R, GPIO_IN);
-    gpio_pull_up(BTN_PIN_R);
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
+void led_2_task(void *p) {
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
 
-    int delay = 0;
+    int delay = 0; // Delay inicial em ms
+    int dummy;
+
     while (true) {
-        if (xSemaphoreTake(xSemaphore_r, pdMS_TO_TICKS(500)) == pdTRUE) {
-            if (delay < 1000) {
+
+        if (xQueueReceive(xQueueButId_g, &dummy, pdMS_TO_TICKS(500)) == pdPASS) {
+            if (delay < 1000)
                 delay += 100;
-            } else {
+            else
                 delay = 100;
-            }
-            printf("delay btn %d \n", delay);
-            xQueueSend(xQueueButId, &delay, 0);
+            printf("Green LED delay updated to: %d ms\n", delay);
+        }
+        if (delay > 0) {
+
+            gpio_put(LED_PIN_G, 1);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+            gpio_put(LED_PIN_G, 0);
+            vTaskDelay(pdMS_TO_TICKS(delay));
         }
     }
 }
 
 int main() {
     stdio_init_all();
-    printf("Start RTOS \n");
+    printf("Start RTOS\n");
 
-    xQueueButId = xQueueCreate(32, sizeof(int));
-    xSemaphore_r = xSemaphoreCreateBinary();
 
-    xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
+    xQueueButId_r = xQueueCreate(32, sizeof(int));
+    xQueueButId_g = xQueueCreate(32, sizeof(int));
+
+
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
+
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+
+
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
+
+
+    xTaskCreate(led_1_task, "LED_R_Task", 256, NULL, 1, NULL);
+    xTaskCreate(led_2_task, "LED_G_Task", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
